796e0b750e065b2993e070fb9b546833b492594d
Dirk E. Rose Dominguez <drosemx@outlook.com>
2021-01-07 12:05:59 -0600

    Add project files.

---
+++ TrianguloPascal/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+
+namespace TrianguloPascal
+{
+    class Program
+    {
+      
+
+        public static void Main()
+        {
+            int j, k, n, r;    /*   variables */
+            int comb, h, i;
+
+
+            Console.WriteLine("Hola Triangulo de Pascal");
+            Console.ReadKey();
+            Console.WriteLine("1");
+
+            for (n = 1; n <= 7; n++)
+            {
+                Console.WriteLine("\n");
+                for (r = 0; r <= 7; r++)
+                {
+                    h = factorial(n);
+                    k = (n - r);
+                    i = factorial(k);
+                    j = factorial(r);
+                    comb = h / (i * j);
+                    if (comb == 0)
+                        Console.Write(" ");
+                    else
+                    {
+                        Console.Write(comb.ToString("g"));
+                    }
+                }
+            }
+            Console.ReadKey();
+        }
+
+
+         static  int factorial(int n)
+         {
+             int fact,c,a;
+             //factorial   
+             if (n == 0)
+             {
+                 fact = 1;
+             }
+             else
+             {
+                 fact = 1;
+                 for (a = 1; a <= n; a++)
+                     fact = fact * a;
+             }
+             c = fact;
+             return c;
+         }
+      
+
+    }
+}
---
+++ TrianguloPascal/TrianguloPascal.aspx.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace TrianguloPascal
+{
+    public partial class TrianguloPascal : System.Web.UI.Page
+    {
+        int fact, a, c, comb, h, i;
+        int j, k, n, r;    /*   variables  globales */
+        public int factorial(int n)
+        {
+            //factorial   
+            if (n == 0)
+            {
+                fact = 1;
+            }
+            else
+            {
+                fact = 1;
+                for (a = 1; a <= n; a++)
+                    fact = fact * a;
+            }
+            c = fact;
+            return c;
+        }
+
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Response.Write("<center><br><br>");
+            Response.Write("****** Triangulo de Pascal *********");
+            Response.Write("<br><br>");
+
+            Response.Write("1");
+            for (n = 1; n <= 7; n++)
+            {
+                Response.Write("<br>");
+                for (r = 0; r <= 7; r++)
+                {
+                    h = factorial(n);
+                    k = (n - r);
+                    i = factorial(k);
+                    
+                    j = factorial(r);
+                    comb = h / (i * j);
+                    if (comb == 0)
+                        Response.Write(" ");
+                    else
+                    {
+                        Response.Write(comb.ToString("g"));
+                    }
+                }
+            }
+            Response.Write("</center><br><br>");
+        }
+    }
+}
